5aac6c42194718ca0e396293ff3b5c288c313260
evgeniyatischenko <jakky@mail.ru>
2022-11-16 17:17:13 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Евгения Тищенко");
+        System.out.println("5 августа 1985 г.");
+    }
+}
